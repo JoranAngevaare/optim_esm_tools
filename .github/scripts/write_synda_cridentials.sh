b83c4cf11240582851d5403a21e63e5f72be49a7
Joran Angevaare <joranangevaare@gmail.com>
2023-05-31 15:35:55 +0200

    add some if fi

--- .github/scripts/write_synda_cridentials.sh
+++ .github/scripts/write_synda_cridentials.sh
@@ -1,4 +1,13 @@
-mkdir $ST_HOME/conf
+synda_conf_dir=$ST_HOME/conf
+
+if [[ -d "$synda_conf_dir" ]]
+then
+    echo "$synda_conf_dir exists on your filesystem."
+else
+    mkdir $synda_conf_dir
+fi
+    
+
 cat << EOF > $ST_HOME/conf/credentials.conf
 [esgf_credential]
 openid = $OPEN_ID
