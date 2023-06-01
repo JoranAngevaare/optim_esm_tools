22b5762a44f94a29ab039f2474683b9e5586efd0
Joran Angevaare <joranangevaare@gmail.com>
2023-06-01 09:14:41 +0200

    add basic test

---
+++ test/test_viewer.py
@@ -0,0 +1,18 @@
+import unittest
+import optim_esm_tools as oet
+import os
+
+
+class TestViewer(unittest.TestCase):
+    def test_basic(self, **kw):
+        viewer = oet.synda_files.synda_files.SyndaViewer(os.path.join(os.environ.get('ST_HOME', '.'), 'data'), **kw)
+        viewer.tree().show()
+
+    def test_max_depth(self):
+        self.test_basic(max_depth=3)
+
+    def test_show_files(self):
+        self.test_basic(show_files=1)
+
+    def test_count_files(self):
+        self.test_basic(count_files=1)
