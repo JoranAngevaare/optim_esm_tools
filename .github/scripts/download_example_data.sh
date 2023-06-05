6c8c232abaa1d4355249f374c5b7e2338462b570
Joran R. Angevaare <joranangevaare@gmail.com>
2023-06-05 13:48:28 +0200

    Update download_example_data.sh

--- .github/scripts/download_example_data.sh
+++ .github/scripts/download_example_data.sh
@@ -6,7 +6,7 @@ synda queue
 
 example_file=$ST_HOME/data/CMIP6/ScenarioMIP/CCCma/CanESM5/ssp585/r3i1p2f1/Amon/tas/gn/v20190429/tas_Amon_CanESM5_ssp585_r3i1p2f1_gn_201501-210012.nc
 
-for n_tries in {1..10}
+for n_tries in {1..6}
 do
     synda queue
     synda watch
@@ -32,5 +32,7 @@ if [[ -f "$example_file" ]]; then
     echo "$example_file exists on your filesystem. Succes"
 else
     # Did not find anything in the downloaded folder
+    echo "perhaphs anything in the logs?"
+    tail $ST_HOME/log/*
     exit -1
 fi
