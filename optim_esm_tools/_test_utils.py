1644dfab99c6c32b052ecf58196e2d7d1e4f0b0d
Joran R. Angevaare <joranangevaare@gmail.com>
2023-06-16 14:16:39 +0200

    Add download routine from pangeo store (#51)
    * solve issue #45
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix test
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add install
    
    * fix fast test
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci

--- optim_esm_tools/_test_utils.py
+++ optim_esm_tools/_test_utils.py
@@ -4,8 +4,41 @@ import os
 EXMPLE_DATA_SET = 'CMIP6/ScenarioMIP/CCCma/CanESM5/ssp585/r3i1p2f1/Amon/tas/gn/v20190429/tas_Amon_CanESM5_ssp585_r3i1p2f1_gn_201501-210012.nc'
 
 
+def get_file_from_pangeo():
+    from xmip.utils import google_cmip_col
+
+    # import cftime
+    col = google_cmip_col()
+    search = col.search(
+        source_id='CanESM5',
+        variable_id='tas',
+        table_id='Amon',
+        experiment_id='ssp585',
+        member_id=['r3i1p2f1'],
+    )
+
+    ddict = search.to_dataset_dict(
+        xarray_open_kwargs={'use_cftime': True},
+    )
+    data = list(ddict.values())[0]
+    # data = data.groupby('time.year').mean('time')
+    # data = data.rename(year='time')
+    data = data.mean(set(data.dims) - {'x', 'y', 'time'})
+    # data['time'] = [cftime.DatetimeNoLeap(y,1,1) for y in data['time']]
+
+    write_to = get_example_data_loc()
+    dest_folder = os.path.split(write_to)[0]
+    os.makedirs(dest_folder, exist_ok=True)
+    if os.path.exists(write_to):
+        print(f'already file at {write_to}')
+        write_to = os.path.join(dest_folder, 'test.nc')
+    data.to_netcdf(write_to)
+
+
 def get_synda_loc():
-    return os.path.join(os.environ.get('ST_HOME'), 'data')
+    return os.path.join(
+        os.environ.get('ST_HOME', os.path.join(os.path.abspath('.'), 'cmip')), 'data'
+    )
 
 
 def get_example_data_loc():
@@ -20,7 +53,9 @@ def synda_test_available():
         return False
     if not os.path.exists(get_example_data_loc()):
         print(f'No {get_example_data_loc()}')
-        return False
+        # Try from pangeo?
+        get_file_from_pangeo()
+        return os.path.exists(get_example_data_loc())
     return True
 
 
