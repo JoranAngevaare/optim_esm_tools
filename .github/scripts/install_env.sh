c89469d9d2d83c9ac88c987c7bc264dcf08d802c
Joran Angevaare <joranangevaare@gmail.com>
2023-06-21 11:32:27 +0200

    fix a few codefactor issues

--- .github/scripts/install_env.sh
+++ .github/scripts/install_env.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-git clone https://github.com/JoranAngevaare/optim_esm_base.git base_install
-cd base_install
-bash install_software.sh $1
-cd ..
+git clone https://github.com/JoranAngevaare/optim_esm_base.git ../base_install
+cd ../base_install || bash install_software.sh $1
+
 pip install -e ../optim_esm_tools
 pip install -r requirements_tests.txt
