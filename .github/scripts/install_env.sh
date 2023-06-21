a63017da8c8909a04703975ea3ad5a8529c85ce0
Joran Angevaare <joranangevaare@gmail.com>
2023-06-21 11:38:34 +0200

    ignore codefactor here

--- .github/scripts/install_env.sh
+++ .github/scripts/install_env.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-git clone https://github.com/JoranAngevaare/optim_esm_base.git ../base_install
-cd ../base_install || bash install_software.sh $1
-
+git clone https://github.com/JoranAngevaare/optim_esm_base.git base_install
+cd base_install
+bash install_software.sh $1
+cd ..
 pip install -e ../optim_esm_tools
 pip install -r requirements_tests.txt
