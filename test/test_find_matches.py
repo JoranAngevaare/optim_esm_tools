f2ecc0acf6f312965f921197933c3f4f93f2f9a2
Joran Angevaare <joranangevaare@gmail.com>
2023-07-12 20:15:53 +0200

    debug

--- test/test_find_matches.py
+++ test/test_find_matches.py
@@ -8,6 +8,7 @@ from optim_esm_tools._test_utils import get_example_data_loc
 class TestMatches(unittest.TestCase):
     def test_find_matches(self):
         path = get_example_data_loc()
+        assert os.path.exists(path), f'no {path}'
         base = path.split('ScenarioMIP')[0]
         head, tail = os.path.split(path)
         kw = oet.analyze.find_matches.folder_to_dict(head)
@@ -16,4 +17,4 @@ class TestMatches(unittest.TestCase):
             required_file=tail,
             **kw,
         )
-        assert len(matches)
+        assert len(matches), dict(base=base, required_file=tail, **kw,)
