a8d50763b0bd34e657675427e06e16c65fc771d2
Joran Angevaare <joranangevaare@gmail.com>
2023-07-12 18:04:21 +0200

    solve conondrum

--- .github/scripts/install_tex.sh
+++ .github/scripts/install_tex.sh
@@ -1,1 +1,1 @@
-sudo apt install texlive texlive-latex-extra texlive-fonts-recommended dvipng cm-super
+sudo apt install texlive texlive-latex-extra texlive-fonts-recommended dvipng cm-super --fix-missing
