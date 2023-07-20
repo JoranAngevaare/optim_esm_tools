286eac5a6fc046538fa80db3e5c5865c32c27860
J.R. Angevaare <joran.angevaare@gmail.com>
2023-07-20 14:23:36 +0000

    Bump version: 1.0.1 → 1.0.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requirements = open_requirements('requirements.txt')
 
 setuptools.setup(
     name='optim_esm_tools',
-    version='1.0.1',
+    version='1.0.2',
     description='Tools for OptimESM',
     long_description=readme + '\n\n' + history,
     long_description_content_type='text/markdown',
