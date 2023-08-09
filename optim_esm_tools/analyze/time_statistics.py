33d5be2557d708e310c26e01636771c8603a05f0
J.R. Angevaare <joran.angevaare@gmail.com>
2023-08-09 07:55:02 +0000

    use file from attrs

--- optim_esm_tools/analyze/time_statistics.py
+++ optim_esm_tools/analyze/time_statistics.py
@@ -15,10 +15,13 @@ class TimeStatistics:
     def calculate_statistics(self) -> ty.Dict[str, ty.Optional[float]]:
         """
         For a given dataset calculate the statistical properties of the dataset based on three tests:
-            1. The max 10-year jump w.r.t. the standard deviation of the piControl run
+            1. The max 10-year jump w.r.t. the standard deviation of the piControl run. Based on
+                yearly means
             2. The p-value of the "dip test" [1]
             3. The p-value of the Skewness test [2]
             4. The p-value fo the symmetry test [3]
+            5. The fraction of the selected regions standard-deviation w.r.t. to the standard
+                deviation of the global average standard-deviation. Yearly means
 
         Citations:
             [1]:
@@ -57,13 +60,11 @@ class TimeStatistics:
         }
 
 
-def n_times_global_std(
-    ds, field='std detrended', min_time=None, max_time=(2100, 12, 30), average_over=None
-):
+def n_times_global_std(ds, field='std detrended', average_over=None, **read_kw):
     average_over = average_over or oet.config.config['analyze']['lon_lat_dim'].split(
         ','
     )
-    ds_global = oet.read_ds(ds.attrs['path'], min_time=min_time, max_time=max_time)
+    ds_global = oet.load_glob(ds.attrs['file'], **read_kw)
     return float(ds[field].mean(average_over) / ds_global[field].mean(average_over))
 
 
@@ -79,7 +80,6 @@ def get_mask_from_global_mask(ds, mask_key='global_mask', rename_dict=None):
 
 def get_historical_ds(ds, _file_name=None, **kw):
     find = oet.analyze.find_matches.associate_historical
-
     find_kw = oet.utils.filter_keyword_arguments(kw, find, allow_varkw=False)
     read_kw = oet.utils.filter_keyword_arguments(kw, oet.read_ds, allow_varkw=False)
     if _file_name is not None:
