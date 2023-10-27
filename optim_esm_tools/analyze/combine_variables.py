f3f17c0ccad7bb6573e638c78785ca0baaaa9bec
J.R. Angevaare <joran.angevaare@gmail.com>
2023-10-27 14:49:50 +0000

    reverse colors

--- optim_esm_tools/analyze/combine_variables.py
+++ optim_esm_tools/analyze/combine_variables.py
@@ -23,7 +23,7 @@ class VariableMerger:
     common_mask: xr.DataArray
 
     _independent_cmaps = imdict(
-        zip(['siconc', 'sos', 'tas', 'tos'], ['Blues', 'Greens', 'Reds', 'Purples']),
+        zip(['siconc', 'sos', 'tas', 'tos'], ['Blues_r', 'Greens_r', 'Reds_r', 'Purples_r']),
     )
     _independent_legend_kw = imdict(
         numpoints=1,
