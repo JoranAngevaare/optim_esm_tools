4fe4e5452ae95df4d4b581be328872a00b961d92
J. R. Angevaare <joranangevaare@gmail.com>
2024-03-14 15:41:34 +0100

    add some named region finding

--- optim_esm_tools/region_finding/__init__.py
+++ optim_esm_tools/region_finding/__init__.py
@@ -2,6 +2,8 @@ from ._base import MaskAll
 from .iter_ranges import *
 from .local_history import LocalHistory
 from .max_region import MaxRegion
+from .named_region import Asia
+from .named_region import NamedRegions
 from .percentiles import Percentiles
 from .percentiles_history import PercentilesHistory
 from .product_percentiles import ProductPercentiles
