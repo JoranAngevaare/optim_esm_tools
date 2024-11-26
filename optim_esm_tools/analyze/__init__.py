fdef3b1cb0fdc9921a2111040d17e43e1427d88c
Joran R. Angevaare <joranangevaare@gmail.com>
2024-11-26 16:23:14 +0100

    Merge adjacent candidates (#199)
    * merge candidate logic
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix imports
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * update precommit
    
    * increase testing
    
    * also test cached version

--- optim_esm_tools/analyze/__init__.py
+++ optim_esm_tools/analyze/__init__.py
@@ -2,9 +2,9 @@ from . import clustering
 from . import cmip_handler
 from . import combine_variables
 from . import concise_dataframe
-
 from . import find_matches
 from . import io
+from . import merge_candidate_regions
 from . import pre_process
 from . import region_finding
 from . import time_statistics
